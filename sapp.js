e870e717607775a1b7eec4ed7298221ce4f7c1e5
Zaid Khan <zaidbinasif468@gmail.com>
2026-02-13 23:06:07 +0500

    changes

--- sapp.js
+++ sapp.js
@@ -1,42 +1,44 @@
 
 //======================================================
-//                   Tasks 1.2
+//                   Tasks 1.1
 //======================================================
 
 
-// let nums = [1, 2, 4,9,12, 5];
+// let nums = [1, 2, 4, 9, 12, 5];
 
-// console.log(`Nums = ${nums}`);
+// console.log(nums);
 
-// console.group();
+// console.group("Family Map");
+
 // const mapResult = nums.map((num, index) => {
-//     console.log(`map step ${index + 1}: Current num = ${num}`);
+//     console.log(`map index ${index}: Current num = ${num}`);
 //     return num * 2;
 // });
-// console.log("Map itrates over the array and return new array don't change the orignal ");
 
+
+// console.log("Map itrates over the array and return new array it don't change the orignal or modifieds it ");
+
+// console.log(mapResult);
 // console.groupEnd();
 
 
 
-// console.group();
-// let step = 1;
-// nums.forEach((num) => {
-//     console.log(`map step ${step}: Current num = ${num}`);
-//     step++
-//     num * 2;
+// console.group("Family forEach");
+// nums.forEach((num, index,orignal) => {
+//     console.log(`Foreach index ${index}: Current num = ${num} result by multipled : ${num * 2}`);
 // });
-// console.log("ForEach does modify or change the array nor do it return the itself  ");
+// console.log("ForEach does modify or change the Original array    ");
 // console.groupEnd();
 
 
+// console.group("Family reduce");
+// const reducedNums = nums.reduce((perivous,next) => {
+//     console.log(`Reduce previous value ${perivous}: Next num = ${next}`);
 
-// console.group();
-// const reducedNums = nums.reduce((acc,num) => {
-//     console.log(`map step ${acc}: Current num = ${num}`);
+//     return perivous + next;
+// });
+// console.log("Reduce does modify or change the Original array but it only give the single value of form the array depending upon you tasks or functions    ");
 
-//     return num * 2;
-// },0);
 // console.log(reducedNums);
 // console.groupEnd();
 
@@ -55,14 +57,14 @@
 // const filterd = nums.filter(num => num > 10);
 // console.log(filterd);
 
-// const Reduce = filterd.reduce((acc, num) => acc + num, 0)
+// const Reduce = filterd.reduce((acc, num) => acc + num)
 // console.log(Reduce);
 
 
 // const filterd2 = nums.filter(num => num < 10);
 // console.log(filterd2);
 
-// const Reduce2 = filterd2.reduce((acc, num) => acc - num, 0)
+// const Reduce2 = filterd2.reduce((acc, num) => acc + num)
 // console.log(Reduce2);
 
 
@@ -72,9 +74,9 @@
 //======================================================
 
 
-// const array = [1,4,7,2,8,2,5]
+// const array = [1,4,7,2,8,2,5];
 
-// function customArray(array, callback) {
+// function customMap(array, callback) {
 //     let result = []
 
 //     for (let i = 0; i < array.length; i++) {
@@ -89,17 +91,10 @@
 // const callBackFunction = (num) => num * 2;
 
 
-// const result = customArray(array, callBackFunction);
+// const result = customMap(array, callBackFunction);
 // console.log(`Custom Map Result ${result}`);
 // const realMap = array.map(callBackFunction  );
 // console.log(`Nromal Map Result ${realMap}`);
-
-
-
-
-
-
-
 
 
 
@@ -112,22 +107,16 @@
 
 
 
-// try {
-//     console.log(`This is My var before decleration${myVar}`);
-//     var myVar = "My Var"
-// } catch (error) {
-//     console.error(error);
+// console.log(`This is My var before decleration ${myVar}`);
+// var myVar = "My Var"
+
+// console.log(`This is My Function before decleration ${myFunction()}`);
+// function myFunction() {
+//     return "Hello Function"
 // }
+
 // try {
-//     console.log(`This is My Function before decleration${myFunction()}`);
-//     function myFunction() {
-//         return "Hello Function"
-//     }
-// } catch (error) {
-//     console.error(error);
-// }
-// try {
-//     console.log(`This is My Let before decleration${myLet}`);
+//     console.log(`This is My Let before decleration ${myLet}`);
 //     let myLet = "My Let"
 // } catch (error) {
 //     console.error(error);
@@ -151,17 +140,17 @@
 
 // function function1() {
 //     console.log("Calling the Function 1");
-// function2()
-//     console.log("Called the Function 1"); \\
+//     function2()
+//     console.log("Called the Function 1"); 
 // }
 // function function2() {
 //     console.log("Calling the Function 2");
-// function3()
+//     function3()
 //     console.log("Called the Function 2");
 // }
 // function function3() {
 //     console.log("Calling the Function 3");
-// function4()
+//     function4()
 //     console.log("Called the Function 3");
 // }
 // function function4() {
@@ -190,4 +179,34 @@
 //======================================================
 //                   Tasks 2.3
 //======================================================
- 
+
+
+// function outerFunction() {
+//     let outVar = "Not Changed"
+//     return function nestedFunction() {
+//         console.log(outVar);
+//         outVar = "Changed"
+//         console.log(outVar  );
+
+//     }
+// }
+// const nestedFunction = outerFunction();
+// nestedFunction()
+
+
+//======================================================
+//                   Tasks 3.1
+//======================================================
+
+const user = {
+    name: "Sir Talal Ahmed",
+    age: 24,
+    country: "Riyadh, Saudi Arabia"
+};
+
+const { name, age, country = "Unknown", gender = "Not Entered" } = user;
+
+console.log(name);
+console.log(age);
+console.log(country);
+console.log(gender); 
